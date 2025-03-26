4641c11023011b8d174ee84de89d7071af2e8347
Dontosquare <Jokke.porsknudsen@gmail.com>
2025-03-26 21:36:43 +0100

    form del 7 backup
    test javascript, beskeder pusher ikke på siden

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+
+const name = document.getElementById('name')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('username')
+const form = getElementById('form')
+const errorElement = document.getElementById('error')
+
+document.addEventListener('DOMContentLoaded', () => {
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null) {
+        messages.push('Venligst udfyld dette felt')
+    }
+
+    if (password.value.lenght <= 6) {
+        messages.push('Kodeordet skal være mere end 6 tegn')
+    }
+
+    if (password.value.lenght >= 6) {
+        messages.push('Kodeordet skal være mindre end 20 tegn')
+    }
+
+    if (password.value === 'password') {
+        message.push('Kodeordet må ikke være password')
+    }
+
+    if (messages.lenght > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+    }
+})})
