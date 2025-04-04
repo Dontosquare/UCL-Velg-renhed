b5cd9bd5d3352c6f40a3035ad7d9a17648fe9cba
madsmads4 <madsolsen213@gmail.com>
2025-04-04 10:30:50 +0200

    mads javascript

--- script.js
+++ script.js
@@ -81,3 +81,47 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 // Sofie slut */
+
+// Mads section
+
+// her laver vi et array som hedder ungerådgiver og putter vores billeder ind
+let ungerådgiver = ['ungerådgiver_billede_2', 'ungerådgiver_billede_1'];
+
+document.addEventListener("click", (Event) => {skift(Event, ungerådgiver)}) // her laver jeg en eventlistener som kalder på vores onClick function med navn skift
+
+// delay function kører også venter den X antal tid
+function delay(ms,) {
+    return new Promise(resolve => setTimeout(resolve,ms)); // laver et nyt "promise" objekt som laver en resolve funktion som bliver kaldt med det simme i "setTimeout" funktionen som venter x anatal tid
+}
+
+// her bruger jeg async funktionen da jeg gerne vil vente på "new promise" objected fra tidligere
+async function displayOneAtATime(time, array) {
+    if (array != null){    // if statement som checker at der er billede i arrayed og hvis ikke der er det bliver der vist "Ingen billeder!" i console
+        
+        for (let i = 0; i < array.length; i++) { // standard for loop 
+            document.getElementById(array[i]).style.opacity = "0"; // her sætter jeg alle billeder i arrayes opacity til 0
+        }
+        
+        for (let i = 0; i < array.length; i++) { // præcis det samme som før standard for loop
+            await delay (time) // her kalder jeg på delayed for at give 2 sekund før den gør billederne synlig
+            document.getElementById(array[i]).style.opacity = "1"; // her sætter jeg alle billeder i arrayes opacity til 1
+        }
+    }
+    else {
+    console.log("Ingen billeder!") // passer til vores if statement fra tidligere
+    }
+}
+displayOneAtATime(2000, ungerådgiver); // her giver jeg funktionen displayOneAtATime et augument på 2000 ms i det tilfælde
+
+function skift(event, array) { // her laver vi en function som vi kalder skift som indebære et parameter
+    let billede_1 = document.getElementById(array[0]) // her laver jeg en variabel som hedder billede_1 hvor vi kalder vores første billede i arrayed
+    let billede_2 = document.getElementById(array[1]) // her gør vi det samme bare med det andet billede
+    if (event.target.id == billede_1.id){ // her laver jeg et if statement som checker at vores "Target id" er samme value som vores billede_1 variable hvis ikke den er det går det bare videre i koden
+        billede_1.style.zIndex = 2 // når vores "Target id" bliver klikket på vil den ændre vores z-index
+        billede_2.style.zIndex = 1 // den her linje gør det samme men ændre bare vores billede_2 z-index
+    }
+    if (event.target.id == billede_2.id){ // den her if statement gør det samme bare reverse
+        billede_1.style.zIndex = 1
+        billede_2.style.zIndex = 2
+    }
+}
